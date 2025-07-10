5eee808d757642458e26e49ebb0686202af91534
Daniel Valcarce <davamix@gmail.com>
2025-07-10 22:06:25 +0200

    Added a simple web ui for AppSample to chat

--- src/AppSample/Program.cs
+++ src/AppSample/Program.cs
@@ -4,20 +4,49 @@ using Microsoft.AspNetCore.Http;
 using AppSample.Services;
 using Microsoft.Extensions.DependencyInjection;
 using System.Threading.Tasks;
+using System;
 
 var builder = WebApplication.CreateBuilder();
 
 builder.Services.AddSingleton<IOllamaService, OllamaService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", async (HttpContext context, IOllamaService ollamaService) => {
-    await foreach (var response in ollamaService.SendMessage("Hello, tell me a joke!"))
+app.UseCors("AllowAll");
+
+app.MapPost("/", async (HttpContext context, IOllamaService ollamaService, HttpRequest request) =>
+{
+    var body = await request.ReadFromJsonAsync<MyRequest>();
+    if (body == null)
+    {
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        await context.Response.WriteAsync("Message cannot be empty.");
+    }
+    Console.WriteLine($"Received message: {body.Message}");
+
+    await foreach (var response in ollamaService.SendMessage(body.Message))
     {
         await context.Response.WriteAsync(response);
         await context.Response.Body.FlushAsync();
         await Task.Delay(1);
+
+        Console.Write(response);
     }
 });
 
 app.Run();
+
+public class MyRequest
+{
+    public string Message { get; set; }
+}
